959e7454057bd7b7bb408a49fea5faabcd366309
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-05-16 23:12:56 +0200

    test: updated weather snapshot & changed apis

--- apps/ui/components/Weather/Weather.spec.js
+++ apps/ui/components/Weather/Weather.spec.js
@@ -8,7 +8,7 @@ const props = {
   city: 'Milan, Italy',
   temperature: 25,
   description: 'Sunny',
-  data: [
+  infos: [
     { label: 'Wind', value: '10km/h' },
     { label: 'Humidity', value: '60%' },
     { label: 'Pressure', value: '1025mb' },
@@ -19,13 +19,11 @@ const props = {
 };
 
 test('should render without crashing', () => {
-  const { data, ...rest } = props;
-  render(<Weather data={data} {...rest} />);
+  render(<Weather data={props} />);
   expect(screen.getByTestId('weather')).toBeInTheDocument();
 });
 
 test('should render correctly', () => {
-  const { data, ...rest } = props;
-  const tree = TestRenderer.create(<Weather data={data} {...rest} />).toJSON();
+  const tree = TestRenderer.create(<Weather data={props} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
